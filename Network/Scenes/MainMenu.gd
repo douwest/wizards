160c26c22a4ae3d201eeead8899e3d29727854b6
douwest <douweheijligers@gmail.com>
2021-11-08 10:27:40 +0100

    create simple dynamic style for menu

--- Network/Scenes/MainMenu.gd
+++ Network/Scenes/MainMenu.gd
@@ -1,13 +1,21 @@
 extends CanvasLayer
 
 
-onready var ipAddress = $Panel/IPAddressInputField
-
+onready var ip_address = $Panel/VBoxContainer/IPAddressInputField
+onready var port_number = $Panel/VBoxContainer/PortNumberInputField
+onready var join_button = $Panel/VBoxContainer/HBoxContainer/Join
 
 func _ready():
 	Network.connect('server_created', self, '_on_ready_to_play')
 	Network.connect("join_success", self, "_on_ready_to_play")
 	Network.connect("join_fail", self, "_on_join_fail")
+
+
+func _process(delta):
+	if !ip_address.text or !port_number.text:
+		join_button.disabled = true
+	else:
+		join_button.disabled = false
 
 
 func _on_ready_to_play():
@@ -19,7 +27,7 @@ func _on_Create_pressed():
 
 
 func _on_Join_pressed():
-	Network.join_server(ipAddress.text, int($Panel/PortNumberInputField.text))
+	Network.join_server(ip_address.text, int(port_number.text))
 	
 
 func _on_join_fail():
