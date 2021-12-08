b573b96b35f56d05f8a5a6b237d634bdca1b0b7f
MoistyKid <larsgramsen@gmail.com>
2021-12-08 16:50:47 +0100

    Revert "Spell"
    This reverts commit d4846402aea02f039b1cc45bc6043cbac58bdd96.

--- Spells/Scripts/Spell.gd
+++ Spells/Scripts/Spell.gd
@@ -1,22 +1,13 @@
 extends Area2D
 
-
 export var bullet_speed = 400
 export var damage = 10
 var speed = 0
 onready var animationPlayer = $AnimationPlayer
 
-<<<<<<< Updated upstream
 func _ready():
 	animationPlayer.play("Start")
 
-=======
-func start(direction: int):
-	if(direction < 0):
-		rotation_degrees = 180
-	animationPlayer.play("Flying")
-	
->>>>>>> Stashed changes
 func _physics_process(delta):
 	position += transform.x * speed * delta
 
